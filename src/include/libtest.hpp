7094ff3c8d0fbc82f5427d009f3f874569726c1a
genuine <genuinebnt@gmail.com>
2023-11-15 10:48:45 +0530

    Project skeleton

---
+++ src/include/libtest.hpp
@@ -0,0 +1,10 @@
+//
+// Created by genuine on 11/15/23.
+//
+
+#ifndef JLOX_LIB_H
+#define JLOX_LIB_H
+
+int add(int a, int b);
+
+#endif //JLOX_LIB_H
---
+++ src/libtest.cpp
@@ -0,0 +1,7 @@
+//
+// Created by genuine on 11/15/23.
+//
+
+int add(int a, int b) {
+    return a + b;
+}
